00700634b9b1bc7d42d1bb4207c68b070ce68516
zombielabsv2 <jindal.rahul@gmail.com>
2024-12-02 00:26:53 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+import React from 'react';
+import NumerologyCalculator from '../components/NumerologyCalculator';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Numerology Calculator</h1>
+      <NumerologyCalculator />
+    </div>
+  );
+};
+
+export default Home;
